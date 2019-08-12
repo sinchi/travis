e3c7109efea063af43e02e6c7a67d3fff0d88475
ayoub <ayoub.belghar@gmail.com>
2019-08-12 15:02:21 +0000

    add deploy to test

--- src/App.test.js
+++ src/App.test.js
@@ -20,3 +20,19 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+/*deploy:
+   provider: elasticbeanstalk
+   region: us.west.aws
+   app: "docker"
+   env: "Docker-env"
+   bucket_name: name of s3
+   bucket_path: name of directory
+   on:
+    - branch: master */
